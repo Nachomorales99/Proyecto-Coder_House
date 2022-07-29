dd5d5c4dde7c498626d8dd89f13978e5de6ed777
Nacho Morales <nacho.morales5@gmail.com>
2022-07-28 23:38:55 -0300

    cambios

---
+++ js/carrito.js
@@ -0,0 +1,47 @@
+//Agregar elementos al carrito 
+
+let carritoDeCompras = []
+
+let cart = (itemId) => {
+    let contenedorCarrito = document.getElementById('contenedor-carrito');
+    let mostrarEnCarrito = () => {
+        let item = stockRemeras.find(item => item.id == itemId)
+
+        carritoDeCompras.push(item);
+        localStorage.setItem("stockRemeras", JSON.stringify(carritoDeCompras));
+
+        let div = document.createElement("div");
+        div.classList.add("productoEnCarrito");
+        div.innerHTML = `<p>${item.nombre}</p> 
+        <img src="${item.img}"></img>
+        <p>Precio: ${item.precio}</p>
+        <button class="btn btn-danger btn-sm" id="delete${item.id}">X</button>`
+
+        contenedorCarrito.appendChild(div);
+
+        let buttonDelete = document.getElementById(`delete${item.id}`);
+        buttonDelete.addEventListener("click", (e) => {
+            borrarProducto(e);
+        })
+        actualizarCarrito();
+    }
+    mostrarEnCarrito();
+}
+
+//Quitar elementos al carrito
+
+function borrarProducto(e) {
+    let btnClicked = e.target;
+    btnClicked.parentElement.remove()
+    actualizarCarrito();
+}
+
+//Actualizar elementos al carrito
+
+function actualizarCarrito() {
+    let contadorCarrito = document.getElementById('contador-carrito');
+    contadorCarrito.innerText = carritoDeCompras.length
+
+    let total = document.getElementById('precioTotal')
+    total.innerText = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
+} 
--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 //Mostrar Productos
 
-let contenedorProductos = document.getElementById('card-ind')
+let mostrarRemeras = (stockRemeras) => {
 
-mostrarRemeras();
+    let contenedorProductos = document.getElementById('card-ind');
 
-function mostrarRemeras() {
     stockRemeras.forEach(item => {
         let div = document.createElement('div')
         div.className = 'col-lg-4 mb-3'
-        div.innerHTML = `<div class="card">
+        div.innerHTML += `<div class="card">
             <div class="img-container">
                 <img src=${item.img} alt="Basic" class="image card-img-top" data-aos="fade-up">
                 <div class="overlay">
-                    <button id="boton ${item.id}" class="btn btn-outline-secondary btn-sm"><i
+                    <button class="btn btn-outline-secondary btn-sm" id="button${item.id}"><i
                             class="fas fa-shopping-cart mr-2">Agregar al carrito</i></button>
                 </div>
             </div>
@@ -25,55 +24,12 @@ function mostrarRemeras() {
 
         contenedorProductos.appendChild(div)
 
-        // let boton = document.getElementById(`boton${item.id}`)
-        // boton.addEventListener("click",()=>{
-        //     cart(item.id);
-        //     alert(`Agregaste ${item.nombre}`)
-        // })
+        let botton = document.getElementById(`button${item.id}`)
+        botton.addEventListener("click",()=>{
+            cart(item.id);
+            alert(`Agregaste ${item.nombre}`)
+        })
     })
 }
 
-//Agregar elementos al carrito
-
-let carritoDeCompras = [] 
-
-let cart = (itemId) =>{
-    let contenedorCarrito = document.getElementById('contenedor-carrito');
-    let mostrarEnCarrito = () => {
-        let producto = stockRemeras.find(producto => producto.id == itemId)
-
-        carritoDeCompras.push(producto);
-        localStorage.setItem("stockRemeras",JSON.stringify(carritoDeCompras));
-
-        let div = document.createElement("div");
-        div.classList.add("productoEnCarrito");
-        div.innerHTML = `<p>${item.nombre}</p> 
-        <p>Precio: ${item.precio}</p>
-        <p id="quantity${item.id}">Quantity</p>
-        <button class="btn btn-danger btn-sm" id="delete${item.id}">X</button>`
-    
-        contenedorCarrito.appendChild(div);
-        let buttonDelete = document.getElementById(`delete${item.id}`);
-        buttonDelete.addEventListener("click",(e)=>{
-            borrarProducto(e);
-        })
-    }
-    mostrarEnCarrito();
-}
-
-let contadorCarrito = document.getElementById('contador-carrito')
-let total = document.getElementById('precioTotal')
-
-//Quitar elementos al carrito
-
-function borrarProducto(e){
-    let btnClicked = e.target;
-    btnClicked.parentElement.remove()
-}
-
-//Actualizar elementos al carrito
-
-function actualizarCarrito() {
-    contadorCarrito.innerText = carritoDeCompras.length
-    total.innerText = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
-}  
+mostrarRemeras(stockRemeras);
